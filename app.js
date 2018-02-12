53a85835f8799effa5d65c34a39a65137f6579ba
durward <durward3@cox.net>
2018-02-11 21:03:54 -0500

    Hopefully changing the port will make heroku work

--- app.js
+++ app.js
@@ -144,5 +144,6 @@ app.get('/refresh_token', function(req, res) {
 });
 
 
-console.log('Listening on 8888');
-app.listen(8888);
+app.listen(process.env.PORT || 3000, function() {
+console.log("LISTENING");
+});
